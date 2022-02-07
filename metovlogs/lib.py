1f854d3d221e2ba560ed1915a60b56bdeeaff754
Azat Akhmetov <azainfo@yandex.com>
2022-02-06 17:59:13 -0800

    get log level from LOG_LEVEL envar

---
+++ metovlogs/lib.py
@@ -0,0 +1,17 @@
+import coloredlogs
+
+import os
+
+
+DEFAULT_LEVEL = "DEBUG"
+LOG_LEVEL_ENVAR_NAME = "LOG_LEVEL"
+
+
+def level_from_envar():
+    """
+    Read log level from environment variable.
+    """
+    level = os.environ.get(LOG_LEVEL_ENVAR_NAME)
+    if level not in coloredlogs.find_defined_levels():
+        level = DEFAULT_LEVEL
+    return level
